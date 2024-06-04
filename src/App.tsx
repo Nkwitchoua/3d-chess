60cbfc3d1d3508480b195ec92f4f8b7766340bd9
unknown <ali.dias.0000@gmail.com>
2024-06-04 18:55:33 +0600

    background, orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { MutableRefObject, useEffect, useRef, useState, Suspense, lazy } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import RotatingElement from './RotatingElement'
 import CameraControl from './CameraControl'
-import { Vector3 } from 'three';
+import { ImageLoader, Scene, TextureLoader, Vector3 } from 'three';
 import Board from './models/Board';
+import sky from "../public/assets/4k-sky-jarxlx0n1mkzmn1j.jpg"
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   const canvas = useRef(null);
+  const scene = new Scene();
+  const skyTexture = useLoader(TextureLoader, "../public/assets/4k-sky-jarxlx0n1mkzmn1j.jpg");
+
+  
+  const created = () => {
+    
+    scene.background = skyTexture;
+  }
 
   return (
     <div id='container' style={{ width: "100vw", height: "100vh" }}>
-      <Canvas ref={canvas} id='canvas' >
+      <Canvas ref={canvas} id='canvas' onCreated={created}>
         <CameraControl/>
+        <OrbitControls/>
         <ambientLight intensity={Math.PI / 2}/>
-        <directionalLight color={"red"} position={[0,0,3]} />
+        {/* <directionalLight color={"red"} position={[0,0,3]} /> */}
         <Suspense fallback={"loading"}>
           <Board/>
+          <primitive attach="background" object={skyTexture}/>
         </Suspense>
       </Canvas>
     </div>
